ebecbfde059445df7c2826550456ffa757ec60b6
Seongmin Park <duke7201@naver.com>
2025-07-12 05:27:06 +0900

    [FEAT] Collect and Transmit real sensor data (#22)
    * feat: add dht libarary
    
    * feat: use sensor data to send message
    
    * feat: init sensors in main code

--- workernode/sensor.h
+++ workernode/sensor.h
@@ -2,6 +2,11 @@
 #define SENSOR_H
 
 #include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 19
+#define DHTTYPE DHT22
+#define PHOTO_PIN 20
 
 enum SensorType {
   SENSOR_NONE,
@@ -10,8 +15,10 @@ enum SensorType {
   SENSOR_HUMIDITY
 };
 
+extern DHT dht;
 extern SensorType currentSensor;
 
+void initSensors();
 void updateDetectionTarget(SensorType target);
 void deactivateAllSensors();
 String readFromCurrentSensor();
