01f7e66af82abf314493cfe3b2f2df1ef40d0d55
JEON MIN JU <96719969+mingmingmon@users.noreply.github.com>
2025-07-11 14:06:58 -0400

    chore : organize workernode directory (#19)
    * chore: remove .cpp and .h files #18
    
    * chore: delete .ino file #18
    
    * chore : make workernode directory #18

--- lora_receive.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
--- setuplora.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void setupLoRa();
-void handleLoRaEvents();
